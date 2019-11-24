de74e1cb6942992090f220ca1dccbf95cb5af628
jiangdahe <jiangdaheda2@sina.com>
2019-11-25 03:14:27 +0800

    add build

---
+++ build/precache-manifest.c8bf65f8d12136b43bf745cd1762215e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0086f32351bdacb6c7b010103c46f96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56f6c93b1aae9a4de916",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2bef55b4905266ee6e77",
+    "url": "/static/js/2.aa8e4a11.chunk.js"
+  },
+  {
+    "revision": "56f6c93b1aae9a4de916",
+    "url": "/static/js/main.6faf7224.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  }
+]);
