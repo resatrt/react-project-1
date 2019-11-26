fa9df12bd9f2434ef62915b0ef98a8f0e2cdb74e
jiangdahe <jiangdaheda2@sina.com>
2019-11-26 19:24:10 +0800

    忘记将build上传

---
+++ build/precache-manifest.36121135848f4973ac28b759481b2618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e42dcbe2ec0f9a142a575c9f57de2c7",
+    "url": "/react-project-1/build/index.html"
+  },
+  {
+    "revision": "b56566a88ab71d38e3af",
+    "url": "/react-project-1/build/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "daaddd7c9ad0ef84f2bf",
+    "url": "/react-project-1/build/static/css/main.6a7904a1.chunk.css"
+  },
+  {
+    "revision": "b56566a88ab71d38e3af",
+    "url": "/react-project-1/build/static/js/2.3619c731.chunk.js"
+  },
+  {
+    "revision": "daaddd7c9ad0ef84f2bf",
+    "url": "/react-project-1/build/static/js/main.7a665a52.chunk.js"
+  },
+  {
+    "revision": "d8093d34ad980e8d6c0b",
+    "url": "/react-project-1/build/static/js/runtime-main.2e2fce96.js"
+  }
+]);
